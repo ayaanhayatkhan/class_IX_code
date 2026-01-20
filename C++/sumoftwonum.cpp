3f3140f51ca4772a58e55a5901105c26ccda2629
Ayaan Hayat <ayaanhayatkhan009@gmail.com>
2026-01-20 20:44:47 +0500

    revised C , C++ and learnt CSS.

---
+++ C++/sumoftwonum.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+cout<<"Enter two numbers";
+int a , b;
+int sum ;
+cin>>a>>b;
+sum = a+b;
+cout<<"Sum of two numbers = "<<sum;
+
+
+
+
+return 0;
+}
