863bb8efa4eb872b92500ff531069a6d5b9449d3
Ayaan Hayat <ayaanhayatkhan009@gmail.com>
2026-01-27 19:03:17 +0500

    Add a table in C and C++ .

---
+++ C++/table_tab.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int tab,a,b;
+int X;
+cout<<"Enter a number :";
+cin>>tab;
+for (a=1;a<=10;a++)
+cout<<"\n"<<tab<< "X" << a << "=" <<tab*a;
+
+
+return 0;
+
+}
---
+++ C/table.c
@@ -0,0 +1,13 @@
+#include<stdint.h>
+int main()
+{
+int tab,a,b;
+printf("Enter a number :");
+scanf("%d",&tab);
+for(a=1;a<=10;a++)
+printf("\n%d X %d = %d",tab,a,tab*a);
+return 0;
+
+
+
+}
