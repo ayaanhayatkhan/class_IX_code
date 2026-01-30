6edfc78f9e0717906ac7113fb73ee2da5f9def44
Ayaan Hayat <ayaanhayatkhan009@gmail.com>
2026-01-30 19:29:37 +0500

    Practice of prcentage and greater number.

---
+++ C++/greatestnumber.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a , b ,c,l;
+cout<<"Enter three numbers";
+cin>>a>>b>>c;
+if (a>b)
+l=a;
+if (l>c)
+cout<<"Greater number is : "<<l;
+else 
+cout<<"Greater number is : "<<c;
+
+
+}
---
+++ C++/percentage2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+float totalMarks , obtainedMarks, Percentage;
+cout<<"Enter totalMarks and obtainedMarks";
+cin>>totalMarks>>obtainedMarks;
+Percentage=obtainedMarks*100/totalMarks;
+cout<<"The Percentage is \t"<<Percentage;
+
+return 0;
+
+
+
+
+}
