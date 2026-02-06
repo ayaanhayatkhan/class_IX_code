5d824b7e1ffc381bf5ad7d8a71d9eaac75448f4b
Ayaan Hayat <ayaanhayatkhan009@gmail.com>
2026-02-06 20:45:20 +0500

    Day 07 of javascript.

---
+++ javascript/Myfunction.js
@@ -0,0 +1,18 @@
+function mytable(a){
+for (let b=1; b<21; b++){
+
+console.log(a + "X" + b + "=" + a*b);
+}
+
+
+}
+
+console.log(mytable(3));
+//console.log("Control return to main");
+
+
+
+
+
+
+
