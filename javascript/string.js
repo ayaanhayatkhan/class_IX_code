5feab0c02758dfa7f290692192450b949a3e14cc
Ayaan Hayat <ayaanhayatkhan009@gmail.com>
2026-02-09 20:52:53 +0500

    Day 10 of javascript.

--- javascript/string.js
+++ javascript/string.js
@@ -3,14 +3,44 @@ console.log(myname.length)
 console.log(myname.toUpperCase())
 console.log(myname.toLowerCase())
 console.log(myname.slice(-5))
-console.log(myname[2])*/
+console.log(myname.replace("Ayaan","Afaan"))*/
+
+
+
 
 // Template Literals
-let a = "Ibrahim"
+/*let a = "Ibrahim"
 let b = "Afaan"
 let sentence = `${a} is the friend of ${b} .`
 console.log(sentence)
 
 // Escape sequence characters
 let fav_fruit = 'Apple\'s'
-console.log(fav_fruit)
+console.log(fav_fruit)*/
+
+
+let a = "Zaryab"
+let b = "Noman"
+console.log(a.concat(" is a friend of " , b ,  " Right ?"))
+let h = "          Azlan            "
+console.log(h.trim())
+console.log(h)
+let text = `He's often called "Ayaan"`;
+console.log(text)
+let text_2 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let length = text.length;
+console.log(length)
+
+
+let d = "Ayaan Hayat"
+console.log(d[0])
+console.log(d[1])
+console.log(d[2])
+console.log(d[3])
+console.log(d[4])
+console.log(d[5])
+console.log(d[6])
+console.log(d[7])
+console.log(d[8])
+console.log(d[9])
+console.log(d[10])
