4af857e4d49d0589cce0dce5df1e8a433000b649
Ayaan Hayat <ayaanhayatkhan009@gmail.com>
2026-02-12 20:43:27 +0500

    Day 13 of javascript.

--- javascript/Array.js
+++
@@ -1,9 +0,0 @@
-let a = [12 , 23 , 43, true , "Hello", false ]
-console.log(a[0])
-console.log(a[1])
-console.log(a[2])
-console.log(a[3])
-console.log(a[4])
-console.log(a[5])
-console.log(typeof a)
-console.log("The length of array is",a.length)
---
+++ javascript/Array_methods.js
@@ -0,0 +1,30 @@
+/*let a = [12 , 23 , 43, true , "Hello", false ]
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+console.log(typeof a)
+console.log("The length of array is",a.length)*/
+
+
+// Array Method
+let a = [1 , 2 , 3 , 6 ]
+let b = a.toString()
+console.log(b, typeof b)
+
+let c = a.join("*")
+console.log(c, typeof c)
+
+let d = a.pop()
+console.log(d, typeof d)
+
+let e = a.push(6)
+console.log(e, typeof e)
+
+let f = a.shift()
+console.log(f)
+
+let g = a.unshift(8)
+console.log(g, typeof g)
