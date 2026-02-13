3d7be7863f4a28ecacc1eee406186c90ea8576e8
Ayaan Hayat <ayaanhayatkhan009@gmail.com>
2026-02-13 21:38:26 +0500

    Day 14 of javascript.

--- javascript/Array_methods.js
+++ javascript/Array_methods.js
@@ -10,7 +10,7 @@ console.log("The length of array is",a.length)*/
 
 
 // Array Method
-let a = [1 , 2 , 3 , 6 ]
+/*let a = [1 , 2 , 3 , 6 ]
 let b = a.toString()
 console.log(b, typeof b)
 
@@ -27,4 +27,30 @@ let f = a.shift()
 console.log(f)
 
 let g = a.unshift(8)
-console.log(g, typeof g)
+console.log(g, typeof g)*/
+
+
+let num = [1 , 2 , 3 , 4 , 5 , 6, 7 , 8 , 9]
+let num_more = [11, 12, 13, 14, 15, 16, 17, 18, 19 ]
+let num_even_more = [21, 22, 23, 24, 25, 26, 27, 28, 29]
+//console.log(num_more)
+//console.log(num.length)
+//delete num[0]
+
+let newarray = num.concat(num_more,num_even_more)
+console.log(newarray)
+console.log(num,num_more)
+
+// sort method
+let compare = (a , b)=>{
+return b-a
+
+}
+
+let num_1 = [53, 34, 23, 42, 24,]
+num_1.sort(compare)
+console.log(num_1)
+
+
+
+
