4862b5929512265495a127379f9198e76a285eeb
Ayaan Hayat <ayaanhayatkhan009@gmail.com>
2026-02-15 20:54:35 +0500

    Practice

---
+++ C++/test.cpp
@@ -0,0 +1,17 @@
+// Write a program to find the addition , multiplication and average
+
+#include<iostream>
+using namespace std;
+int main()
+
+{
+ int a ;
+ int b ; 
+cout<<"Enter two numbers ";
+cin>>a;
+cin>>b;
+cout<<"The sum is " <<a + b<<"\n";
+cout<<"The Multiply is " <<a * b<<"\n";
+cout<<"The Average is "<<(a+b)/2;
+
+}
