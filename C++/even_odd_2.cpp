410799a031f64b7da16f66dc5a6104fa3d9a1c4a
Ayaan Hayat <ayaanhayatkhan009@gmail.com>
2026-02-24 22:38:42 +0500

    New program in C and C++ .

---
+++ C++/even_odd_2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+int a , b;
+cout<<"Enter a number =";
+cin>>a;
+b = a%2;
+if (b>0)
+{
+cout<<"The number is odd ";
+
+}
+else
+{
+cout<<"The number is even ";
+
+}
+
+}
---
+++ C++/smallest.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a , b , c , s;
+cout<<"Enter three numbers = ";
+cin>>a>>b>>c;
+if (a<b)
+s=a;
+else
+s=b;
+if ((s<c))
+cout<<"The smallest number is "<<s;
+else
+cout<<"The smallest number is "<<c;
+
+
+return 0;
+
+
+
+
+
+
+
+}
