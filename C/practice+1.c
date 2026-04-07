548bd4a420689adb7f13dd87cd85800e15d79309
Ayaan Hayat <ayaanhayatkhan009@gmail.com>
2026-04-07 22:07:53 +0500

    Revision

---
+++ C/practice+1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int a  , b ;
+printf("Enter two numbers");
+scanf("%d%d",&a,&b);
+if (a>b)
+
+{
+printf("The greater number is %d",a);
+
+}
+else
+printf("The greater number is %d",b);
+
+return 0;
+
+
+
+}
