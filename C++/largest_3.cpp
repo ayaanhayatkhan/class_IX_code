2a56e0748316662d023c6238cc910a28eaf963ba
Ayaan Hayat <ayaanhayatkhan009@gmail.com>
2026-04-12 23:51:57 +0500

    Largest number

---
+++ C++/largest_3.cpp
@@ -0,0 +1,30 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+float n1, n2, n3;
+cout<<"Enter three number :";
+cin>>n1>>n2>>n3;
+if(n1>=n2){
+
+if (n1>=n3)
+cout<<"Largest number:"<<n1;
+else
+cout<<"Largest number:"<<n3;
+
+}
+
+else{
+if (n2>=n3)
+cout<<"Largest number:"<<n2;
+else
+cout<<"largest number:"<<n3;
+
+}
+
+
+
+return 0;
+
+}
