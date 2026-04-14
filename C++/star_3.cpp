457474eeb7879f8f20a9d5b7d0262bec2078e38b
Ayaan Hayat <ayaanhayatkhan009@gmail.com>
2026-04-14 23:43:55 +0500

    Star program

---
+++ C++/star_3.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+int i = 0;
+for (i=0;i<5;i++)
+{
+    for(int j=0;j<=i;j++)
+    {cout<<"*";}
+cout<<endl;
+}
+
+
+//------------------------
+
+for (int x=4;x>=0;x--)
+{
+    for(int y=0;y<=x;y++)
+ {cout<<"*";}
+cout<<"*"<<endl;
+}
+
+
+
+
+
+return 0;
+
+}
