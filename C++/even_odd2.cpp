1c9455c6cc47a5bd7782c5a1e256c726622a1110
Ayaan Hayat <ayaanhayatkhan009@gmail.com>
2026-04-15 22:35:56 +0500

    Even and odd

---
+++ C++/even_odd2.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a ;
+int b ;
+cout<<"Enter a munber = ";
+cin>>a;
+
+b = a%2;
+if (b>0)
+{
+    cout<<"The number is odd";
+
+}
+
+else  
+{
+    cout<<"The number is even";
+
+}
+
+
+
+}
