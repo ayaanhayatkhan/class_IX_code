8f5dbe206fc28bbda35a484cf5ef31e9c43efd45
Ayaan Hayat <ayaanhayatkhan009@gmail.com>
2026-05-01 22:21:34 +0500

    Finding age

---
+++ C/sum2num.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a, b;
+printf("\nEnter first number=");
+scanf("%d",&a);
+
+printf("\nEnter second number=");
+scanf("%d",&b);
+
+if (a==b)
+printf("Both number are equal");
+else
+printf("Both number are not equal");
+
+
+}
