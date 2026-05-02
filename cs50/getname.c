ae33bef89babf8c5dbb30a59f4751a4c0cbdeeaf
Ayaan Hayat <ayaanhayatkhan009@gmail.com>
2026-05-02 22:56:29 +0500

    Day 2 of  Cs50

---
+++ cs50/getname.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+char name[20];
+
+printf("Enter your name ");
+gets(name);
+printf("Your name is = ");
+puts( name);
+
+char fathername[30];
+int main()
+ string fathername = get_string("What's your name ");
+printf("Hello ,%s\n", fathername);
+
+
+
+
+return 0;
+
+}
