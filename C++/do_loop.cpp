3c002a36b08c7961b26463f7bb59fea3b4818c99
Ayaan Hayat <ayaanhayatkhan009@gmail.com>
2026-05-08 23:43:55 +0500

    C++

---
+++ C++/do_loop.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+int a , b ;
+b = 0;
+for ( b = 0; b < 3; b++)
+{
+
+
+    cout<<" \n Enter a number ";
+cin>>a;
+if (a%2 == 0)
+cout<<" \n The number is even ";
+else 
+cout<<" \n The number is odd ";
+
+b++;
+
+}
+
+
+
+
+
+return 0;
+
+}
